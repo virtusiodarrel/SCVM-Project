bd98e6564cdada0434c1652efb4323674b000afa
eugenelabuac <eugenelabuac@gmail.com>
2022-09-23 00:57:58 +0800

    functions_added

--- SCVMWebsite/QueryService/views.py
+++ SCVMWebsite/QueryService/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import BDSAForm
+from .models import BDSA
 
 # Create your views here.
 def home(request):
