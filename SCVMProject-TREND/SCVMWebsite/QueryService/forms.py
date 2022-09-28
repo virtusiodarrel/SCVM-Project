63d6716c4247ff48b7282ce33895e632070029e9
eugenelabuac <eugenelabuac@gmail.com>
2022-09-28 16:48:32 +0800

    display_raw_json

--- SCVMProject-TREND/SCVMWebsite/QueryService/forms.py
+++ SCVMProject-TREND/SCVMWebsite/QueryService/forms.py
@@ -5,30 +5,22 @@ from .models import BDSA
 class BDSAForm(ModelForm):
     class Meta:
         model = BDSA
-        fields = ("cve_id", "bdsa_id", "title", "description", "technical_description", "solution", "patch_links", "mitigation", "vul_function")
+        fields = ("cve_id", "bdsa_id", "title", "json_raw")
 
         labels = {
             "cve_id": '',
             "bdsa_id": '',
             "title": '',
-            "description": '',
-            "technical_description": '',
-            "solution": '',
-            "patch_links": '',
-            "mitigation": '',
-            "vul_function": ''
+            "json_raw": '',
+
         }
 
         widgets = {
             "cve_id": forms.TextInput(attrs={'class':'form-control-sm', 'placeholder': 'CVE ID'}),
             "bdsa_id": forms.TextInput(attrs={'class':'form-control-sm', 'placeholder': 'BDSA ID'}),
             "title": forms.TextInput(attrs={'class':'form-control-sm', 'placeholder': 'Title'}),
-            "description": forms.TextInput(attrs={'class':'form-control-sm', 'placeholder': 'Description'}),
-            "technical_description": forms.TextInput(attrs={'class':'form-control-sm', 'placeholder': 'Technical Description'}),
-            "solution": forms.TextInput(attrs={'class':'form-control-sm', 'placeholder': 'Solution'}),
-            "patch_links": forms.TextInput(attrs={'class':'form-control-sm', 'placeholder': 'Patch Links'}),
-            "mitigation": forms.TextInput(attrs={'class': 'form-control-sm', 'placeholder': 'Mitigation'}),
-            "vul_function": forms.TextInput(attrs={'class':'form-control-sm', 'placeholder': 'Vulnerable Function'}),
+            "json_raw": forms.TextInput(attrs={'class':'form-control-sm', 'placeholder': 'JSON Raw Contents'}),
+
         }
 
 class UploadFileForm(forms.Form):
--- SCVMProject-TREND/SCVMWebsite/QueryService/views.py
+++ SCVMProject-TREND/SCVMWebsite/QueryService/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.contrib import messages
 from .forms import BDSAForm
 from .forms import UploadFileForm
 from .models import BDSA
 import json
 from django.core.paginator import Paginator
+from django.forms.models import model_to_dict
 
 # Create your views here.
 def home(request):
-    return render(request, 'QueryService/home.html', {})
+    last_five = BDSA.objects.all().order_by('?')[:5]
+    return render(request, 'QueryService/home.html', {'cve': last_five})
 
 
 def search_cve(request):
     if request.method == "POST":
         searched = request.POST['searched']
         cves = BDSA.objects.filter(cve_id__contains=searched)
-        return render(request, 'QueryService/search_cve.html', {'searched':searched, 'cves':cves})
+        return render(request, 'QueryService/show_cve.html', {'searched':searched, 'cves':cves})
     else:
         return render(request, 'QueryService/search_cve.html', {})
 
@@ -31,7 +33,7 @@ def list_cve(request):
     return render(request, 'QueryService/list_cve.html', {'cve_list': cve_list, 'cves': cves, 'nums':nums})
 
 def show_cve(request, cve_id):
-    cve = BDSA.objects.get(pk=cve_id)
+    cve = BDSA.objects.get(cve_id=cve_id)
     return render(request, 'QueryService/show_cve.html', {'cve': cve})
 
 def upload_json(request):
@@ -57,13 +59,12 @@ def show_json(file, duplicate, added):
     details = file.read()
     bdsa_id = json.loads(details)['name']
     title = json.loads(details)['title']
-    desc = json.loads(details)['description']
-    tech_desc = json.loads(details)['technicalDescription']
-    solution = json.loads(details)['solution']
+    content = json.loads(details)
+    content_formatted = json.dumps(content, indent=3)
     if (BDSA.objects.filter(cve_id=cve_id).exists() or BDSA.objects.filter(bdsa_id=bdsa_id).exists()):
         duplicate.append(cve_id)
     else:
-        BDSA.objects.create(cve_id=cve_id, bdsa_id=bdsa_id, title=title, description=desc, technical_description=tech_desc, solution=solution)
+        BDSA.objects.create(cve_id=cve_id, bdsa_id=bdsa_id, title=title, json_raw=content_formatted)
         added.append(cve_id)
-    return
+    return 
 
